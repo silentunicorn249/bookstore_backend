f0c6c995116ec13ae9ced2e9efbc1569e9476063
markbasssem <mark.bassem@thirdwayv.com>
2023-09-09 00:49:45 +0300

    completed auth, i guess

--- routes/auth.ts
+++ routes/auth.ts
@@ -5,29 +5,32 @@ import { sign } from "jsonwebtoken";
 
 const auth = Router();
 
-
 auth.post("/signin", async (req: Request, res: Response) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  console.log("Got requesst with body", req.body);
+  const username = req.body.username;
+  const password = req.body.password;
 
-    const user = await User.findOne({username: username})
-    if (!user) return res.status(400).send("Invalid username")
+  const user = await User.findOne({ username: username }).select("-__v");
+  if (!user) return res.status(400).send("Invalid username");
 
-    const valid = await compare(password, user.hashedPass)
-    if (!valid) return res.status(400).send("Invalid pass")
+  const valid = await compare(password, user.hashedPass);
+  if (!valid) return res.status(400).send("Invalid pass");
 
-    const token = sign({_id: user._id, type: user.type}, "key")
-    res.header("x-auth-token", token).send(true)
-
+  const token = sign({ _id: user._id, type: user.type }, "key");
+  const tempObj = user.toObject();
+  delete tempObj._id;
+  delete tempObj.hashedPass;
+  console.log("Sending", tempObj);
+  tempObj["token"] = token
+  res.header("x-auth-token", token).send(tempObj);
 });
 auth.post("/signup", async (req: Request, res: Response) => {
   const username = req.body.username;
   const password = req.body.password;
   const type = req.body.type;
 
-  const isExists = await User.findOne({username: username})
-  if(isExists) return res.status(401).send("Username already exists")
-
+  const isExists = await User.findOne({ username: username });
+  if (isExists) return res.status(401).send("Username already exists");
 
   const salt = await genSalt(10);
   const hashedPass = await hash(password, salt);
